0426421888167b13716e0949228c401482a56462
Jordan Jordanov <d_yugioh@abv.bg>
2016-11-25 14:39:24 +0200

    Best Practices and Architecture

---
+++ LCTR-Best Practices and Architecture/Softuni.Data/Contracts/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Softuni.Data.Contracts
+{
+    using System.Linq.Expressions;
+
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        void DeleteRange(IEnumerable<TEntity> entitites);
+
+        TEntity GetById(int id);
+
+        IEnumerable<TEntity> FindAll(Expression<Func<TEntity, bool>> where);
+
+        TEntity Single(Expression<Func<TEntity, bool>> where);
+
+        TEntity SingleOrDefault(Expression<Func<TEntity, bool>> where);
+    }
+}
